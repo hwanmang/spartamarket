6b0ab8565787e660b44ad98b85c5a4e89b90227e
SJ <yws7236@gmail.com>
2024-04-17 15:19:22 +0900

    CREATE: 회원정보_비밀번호변경_아이콘추가
    수정했습니다.

--- spartamarket/accounts/forms.py
+++ spartamarket/accounts/forms.py
@@ -1,9 +1,28 @@
-from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.urls import reverse
 from .models import User
+from django.contrib.auth import get_user_model
 
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = User
         fields = UserCreationForm.Meta.fields
+
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta:
+        model = get_user_model()
+        fields = {
+            "first_name",
+            "last_name",
+            "email"
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if self.fields.get("password"):
+            password_help_text = (
+                "You can change the password " '<a href="{}">here</a>.'
+            ).format(f"{reverse('accounts:change_password')}")
+            self.fields["password"].help_text = password_help_text
--- spartamarket/products/urls.py
+++ spartamarket/products/urls.py
@@ -4,9 +4,9 @@ from products import views
 app_name = "products"
 urlpatterns = [
     path('', views.index, name="index"),
-    path('list_view/', views.list_view, name="list"),
-    path('<int:product_id>/', views.detail_view, name="detail"),
-    path('create/', views.create_view, name="create"),
-    path('update/<int:product_id>/', views.update_view, name="update"),
-    path('delete/<int:product_id>/', views.delete_view, name="delete"),
+    path('list_view/', views.list, name="list"),
+    path('<int:product_id>/', views.detail, name="detail"),
+    path('create/', views.create, name="create"),
+    path('update/<int:product_id>/', views.update, name="update"),
+    path('delete/<int:product_id>/', views.delete, name="delete"),
 ]
