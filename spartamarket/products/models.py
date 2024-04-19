ed2b56f55c48beaa86f3b527aad559896784e602
SJ <yws7236@gmail.com>
2024-04-19 16:39:41 +0900

    좋아요_하트추가

--- spartamarket/products/models.py
+++ spartamarket/products/models.py
@@ -18,12 +18,3 @@ class Product(models.Model):
 
     def __str__(self) -> str:
         return self.title
-
-
-class ProductLike(models.Model):
-    product = models.ForeignKey(
-        Product, on_delete=models.CASCADE, related_name="likes"
-    )
-    user = models.ForeignKey(
-        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="likes"
-    )
